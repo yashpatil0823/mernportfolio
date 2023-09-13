a6a6de6d7c5148d13a767627f9da7feffb594484
Yash Patil <100871133+yashpatil0823@users.noreply.github.com>
2023-09-13 23:42:23 +0530

    Update contactModel.js

--- models/contactModel.js
+++ models/contactModel.js
@@ -8,7 +8,7 @@ const contactSchema = new mongoose.Schema({
     },
     email:{
         type: String,
-        unique:true,
+        // unique:true,
         required:true,
         validate(value){
             if(!validator.isEmail(value)){
